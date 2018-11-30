76ed1f6fcbdf3bf6f83b167adba4db0f1fe1415a
Kars Rhyder <karsrhyder@gmail.com>
2018-11-30 01:09:51 +0100

    more meta

--- index.js
+++ index.js
@@ -132,6 +132,11 @@ async function indexItem(url, key) {
     <html>
     <head>
       <title>`+prettyTitle+`</title>
+
+      <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+      <meta http-equiv="Pragma" content="no-cache" />
+      <meta http-equiv="Expires" content="0" />
+
         <meta name="twitter:card" content="summary_large_image"/>
         <meta name="twitter:description" content="`+prettyDescription+`"/>
         <meta name="twitter:title" content="`+prettyTitle+`"/>
